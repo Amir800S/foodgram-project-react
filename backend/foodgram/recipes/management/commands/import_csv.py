e1864cea4ca03239f9ffb336e63f49aef33051fa
A.M <113894011+Amir800S@users.noreply.github.com>
2023-11-10 10:13:33 +0300

    review 4 attempt

--- backend/foodgram/recipes/management/commands/import_csv.py
+++ backend/foodgram/recipes/management/commands/import_csv.py
@@ -29,23 +29,23 @@ class Command(BaseCommand):
                     if created:
                         self.stdout.write(
                             self.style.SUCCESS(
-                                f'Ингредиент'
-                                f' {ingredient.name} добавлен в базу.'
+                                f"Ингредиент"
+                                f" {ingredient.name} добавлен в базу."
                             )
                         )
                     else:
                         self.stdout.write(
                             self.style.SUCCESS(
-                                f'Ингредиент'
-                                f' {ingredient.name} уже есть в базе.'
+                                f"Ингредиент"
+                                f" {ingredient.name} уже есть в базе."
                             )
                         )
         except FileNotFoundError:
             raise CommandError(
-                'Файл ingredients не найден по указанному пути.'
+                "Файл ingredients не найден по указанному пути."
             )
         try:
-            with open(csv_tags_file_path, 'r', encoding='utf-8') as ing_file:
+            with open(csv_tags_file_path, "r", encoding="utf-8") as ing_file:
                 reader = csv.reader(ing_file, delimiter=',')
                 for name, slug, color in reader:
                     tag, created = Tag.objects.get_or_create(
@@ -55,13 +55,13 @@ class Command(BaseCommand):
                     )
                     if not created:
                         self.stdout.write(self.style.WARNING(
-                            f'Тег {tag.name} уже есть в базе. Обновлено.'))
+                            f"Тег {tag.name} уже есть в базе. Обновлено."))
                     else:
                         self.stdout.write(
                             self.style.SUCCESS(
-                                f'Тег {tag.name} добавлен в базу.')
+                                f"Тег {tag.name} добавлен в базу.")
                         )
         except FileNotFoundError:
             raise CommandError(
-                'Файл tags не найден по указанному пути.'
+                "Файл tags не найден по указанному пути."
             )
